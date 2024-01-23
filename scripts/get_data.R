d01e53c1c38ef7acd87ef2a38bf5567e9e106632
limnoliver <soliver@usgs.gov>
2024-01-23 13:04:41 -0600

    small updates to grab first and last 50 years, filter out incomplete years.

--- scripts/get_data.R
+++ scripts/get_data.R
@@ -6,6 +6,8 @@ library(cowplot)
 
 data <- read_sheet('https://docs.google.com/spreadsheets/d/1uls2NIoxZqB_r64RZSHkW5v9XUBnOglKHSiNElduZzg/edit?usp=sharing', col_types = 'nnccn')
 
+# filter any incomplete years
+data <- filter(data, !is.na(OPENED))
 # create dates
 data$closed_date <- as.Date(paste(data$CLOSED, data$`START YEAR`), format = '%d %b %Y')
 
@@ -42,7 +44,7 @@ plot_dat <- mutate(plot_dat, days_cat = cut(days_of_ice, breaks = c(0, 30, 60, 9
 
 subtitle_1 <- 'Prior to 1900, 4 months of ice cover on Lake Mendota was relatively common (19 of 45 years on record).
 But the last time 4 months of ice was recorded on Lake Mendota was 1977.
-In 1982, Lake Mendota had less than 2 months of ice, an event that had never occured before and has now been recorded 5 times. (Red lines indicate years with multiple freeze/thaw dates.)'
+In 1982, Lake Mendota had less than 2 months of ice, an event that had never occured before and has now been recorded 5 times. Red lines indicate years with multiple freeze/thaw dates.'
 
 subtitle_1 <- paste0(strwrap(subtitle_1, 150), sep="", collapse="\n")
 
@@ -60,28 +62,28 @@ p <- ggplot(plot_dat, aes(x = year, y = days_cat)) +
 plot_dat_long <- mutate(plot_dat, 
                         period = case_when(
                           year < 1906 ~ '1855-1905',
-                          year >= 1906 & year < 1971 ~ '1906-1970',
-                          year >= 1971 ~ '1971-2021')) %>%
+                          year >= 1906 & year < 1972 ~ '1906-1971',
+                          year >= 1972 ~ '1972-2022')) %>%
   tidyr::pivot_longer(c(ice_on, ice_off), names_to = 'metric', values_to = 'day')
 
 # create groups for period + metric
 plot_dat_long <- mutate(plot_dat_long, 
                                 metric_group = case_when(
                                   period == '1855-1905' & metric == 'ice_on' ~ '1855-1905 ice on',
-                                  period == '1906-1970' & metric == 'ice_on' ~ '1906-1970 ice on',
-                                  period == '1971-2021' & metric == 'ice_on' ~ '1971-2021 ice on',
+                                  period == '1906-1971' & metric == 'ice_on' ~ '1906-1971 ice on',
+                                  period == '1972-2022' & metric == 'ice_on' ~ '1972-2022 ice on',
                                   period == '1855-1905' & metric == 'ice_off' ~ '1855-1905 ice off',
-                                  period == '1906-1970' & metric == 'ice_off' ~ '1906-1970 ice off',
-                                  period == '1971-2021' & metric == 'ice_off' ~ '1971-2021 ice off'
+                                  period == '1906-1971' & metric == 'ice_off' ~ '1906-1971 ice off',
+                                  period == '1972-2022' & metric == 'ice_off' ~ '1972-2022 ice off'
                                 ))
 
 # test how much later/earier ice on/ice off was over test periods
 ice_on_early <- median(filter(plot_dat_long, metric_group %in% '1855-1905 ice on') %>% pull(day))
-ice_on_late <- median(filter(plot_dat_long, metric_group %in% '1971-2021 ice on') %>% pull(day))
+ice_on_late <- median(filter(plot_dat_long, metric_group %in% '1972-2022 ice on') %>% pull(day))
 ice_off_early <- median(filter(plot_dat_long, metric_group %in% '1855-1905 ice off') %>% pull(day))
-ice_off_late <- median(filter(plot_dat_long, metric_group %in% '1971-2021 ice off') %>% pull(day))
+ice_off_late <- median(filter(plot_dat_long, metric_group %in% '1972-2022 ice off') %>% pull(day))
 
-subtitle_2 <- 'Both later freeze and earlier thaw dates appear to be contributing to the shorter ice season when comparing median dates of the first and last 50 years of the record. Lake Mendota froze 10.5 days later and thawed 13.5 days earlier.'
+subtitle_2 <- 'Both later freeze and earlier thaw dates appear to be contributing to the shorter ice season when comparing median dates of the first and last 50 years of the record. Lake Mendota froze 10.5 days later and thawed 13 days earlier.'
 
 subtitle_2 <- paste0(strwrap(subtitle_2, 150), sep="", collapse="\n")
 
@@ -105,11 +107,12 @@ p1 <- ggplot(plot_dat_long, aes(x = day)) +
   theme_classic() +
   scale_x_continuous(breaks = c(30, 61, 92, 120, 151, 181), labels = c('Dec 1', 'Jan 1', 'Feb 1', 'March 1', 'April 1', 'May 1')) +
   labs(title = "Later winters and earlier springs",
-       subtitle = subtitle_2, x = '') +
+       subtitle = subtitle_2, y = 'Density', x = 'Figure by Sam Oliver') +
   coord_cartesian(ylim = c(0, 0.05)) +
   geom_text(aes(x = 50, y = 0.048, label = 'Freeze'), size = 4) +
   geom_text(aes(x = 155, y = 0.048, label = 'Thaw'), size = 4) +
-  theme(plot.title = element_text(face = 'bold'))
+  theme(plot.title = element_text(face = 'bold'),
+        axis.title.x = element_text(hjust = 1, vjust = -0.5, face = 'italic', size = 10))
 
 # pull simplified legend from another plot
 p2 <- ggplot(plot_dat_long, aes(x = day)) +
@@ -118,7 +121,7 @@ p2 <- ggplot(plot_dat_long, aes(x = day)) +
   scale_color_manual(values = c('black', NA, 'black')) +
   geom_vline(aes(linetype = 'median freeze/thaw', xintercept = 50)) +
   scale_linetype_manual(values = 2) +
-  labs(linetype = '', fill = '', color = '') +
+  labs(linetype = '', fill = '', color = '', y = 'Density') +
   theme_classic() +
   theme(legend.margin = margin(-0.7,0,0,0, unit="cm"))
 
